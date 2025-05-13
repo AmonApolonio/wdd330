e4bcce201f5ff130882dbd169cdb27688baa4568
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-05-12 21:33:05 -0300

    fix: product details loading page data fix

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import { getParam } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productID = getParam("product");
+const productID = getParam("products");
 const product = new ProductDetails(productID, dataSource);
 product.init();
 
